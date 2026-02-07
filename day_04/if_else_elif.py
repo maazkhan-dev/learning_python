066fc76b27b6cffccc0f9bbebdc92d9044b512b9
maazkhan-dev <maaz14202@gmail.com>
2026-02-07 20:32:35 +0500

    learned inputVar,if_rlde_elif & Loops

---
+++ day_04/if_else_elif.py
@@ -0,0 +1,20 @@
+age = 19
+allowed_age = 18
+
+#  Is he/She is allowed to access this site ?
+#  if (age == allowed_age) :  #or simply   age==allowed_age:
+#     print("You are allowed to access this site.")
+# else:
+#  print("You are not allowed to access this site.")
+
+if (age == allowed_age) :  #or simply   age==allowed_age:
+    print("You are allowed to access this site.")
+elif (age > allowed_age) :                               
+ print("You are 'STRONGLY' allowed to access this site.")
+  # You can write multiple elif statement
+if ( age < 10) :
+  print("You are a Kid! Can't access.")
+else:
+ print("You are not allowed to access this site.")
+
+ 
