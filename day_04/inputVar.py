3cc23f76efef402a555a3c2e7a845a743b45cb69
maazkhan-dev <maaz14202@gmail.com>
2026-02-08 22:39:52 +0500

    if_else_elif, inputVar and loops

---
+++ day_04/inputVar.py
@@ -0,0 +1,22 @@
+# fruit = 'apple'
+# print(fruit)
+
+# using input function
+fruit = input("Enter your favorite fruit: ")
+print(fruit)
+
+#input function 2nd stage
+name = input("Enter your name: ")
+greeting= 'Hello!'
+print(greeting , name)
+
+#Or
+
+# name = input("Enter your name: ")
+# print('Hello' , name)
+
+# 3rd stage of Input fnction
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+greeting = 'Hello!'
+print(greeting , name , ', you are', age, 'years old')
