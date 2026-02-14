4eb9bd57de52ad4f2cd83475e5297b7735ff6da7
maazkhan-dev <maaz14202@gmail.com>
2026-02-14 12:57:42 +0500

    arrays & range

---
+++ day_10/Range.py
@@ -0,0 +1,13 @@
+# creating a range of no. 
+x = range(0,10)
+print (x)
+
+print(list(x)) #Converting to list for printing
+
+y = range(3, 20, 2) #with Step 2
+print(list(y))
+
+# Printing Table 5
+table_05= range(5,55,5)
+print(list(table_05))
+
---
+++ day_10/array.py
@@ -0,0 +1,15 @@
+#defining Array
+samosa = ['aalo', 'pyaaz','chana','masala','namak'] #can hold many values under a single name
+print(samosa)
+
+# Accessing Array elements
+x =samosa[0]
+print(x)
+
+#Adding new element
+samosa[0]='pyaaz'
+print(samosa)
+
+#will print the size of arrray
+z= len(samosa)
+print(z)
