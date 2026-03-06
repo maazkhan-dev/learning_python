a5960762f633572dc57b1ed2d150dc601bddd3f4
maazkhan-dev <maaz14202@gmail.com>
2026-03-06 15:00:30 +0500

    Pie chats

--- day_03/comments.py
+++ day_03/comments.py
@@ -1,13 +1,11 @@
 # subject of this python script :
-# Authored by : Mr. Maaz ML engr
-# where to contact : github profile Link or Email
+# Authored by : Mr. Maaz ML enthusiast
 # viral reel mera dil ye pukarai ajaa hahaha yee chezay nahi lekhni 
 
 # write every thing stepwise
-
 #step : 01
      # why we use single quatatin?
-print('maaz!') # we use for string
+print('UET Peshawar!') # we use for string
 
 # step : 02   why we use double quatation? 
 print ("what's up?") #when we writing string and use of other quotation inside
@@ -15,10 +13,9 @@ print ("what's up?") #when we writing string and use of other quotation inside
 #step : 03
    # when to  use triple '''    ''' quotataion
 print('''
-      "maaz!" 
+      "UET Peshawar" 
       what's up?    
       ''')  # to write multiline and use of ' or "  inside
 
-# Assignment : when to use comments in python? mention 10 cases
   # commenting is very very important in coding for proper commenting that others programming can read your code easily
   
